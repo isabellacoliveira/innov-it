b49baa6807bfa1062f3f142031cc9f762c2fb623
Isaah <isaaholiveira15@gmail.com>
2023-09-24 09:14:10 -0300

    em andamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'innov-it';
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  scrollToTeamSection() {
+    const teamSection = this.el.nativeElement.querySelector('#team-section');
+    if (teamSection) {
+      teamSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
